9894998dadc1e96507b25fb579c335b725a5e277
unknown <vmehar13@gmail.com>
2021-03-30 01:42:38 +0530

    loading gif & fixed some errors

--- client/client.js
+++ client/client.js
@@ -1,6 +1,10 @@
-const form = document.querySelector('.cani')
+//storing value of form
+const form = document.querySelector('.cani');
+const loading = document.querySelector('.loading');
+ //set value of loading none.
+ loading.style.display = 'none';
 
-form.addEventListener('click', (e) => {
+form.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(form);
     const name = formData.get('name');
@@ -10,5 +14,7 @@ form.addEventListener('click', (e) => {
         name,
         content
     };
-    console.log('he;; np')
+    console.log(cani)
+    form.style.display = 'none';
+    loading.style.display = '';
  })
